098b4a7f54642ef039ab1587bf4d314030ff1470
Brian Marquis <rbmarquis.dev@gmail.com>
2024-04-04 19:08:20 -0500

    create initial weather app

---
+++ src/components/search/index.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+export default function Search({ search, setSearch, handleSearch }) {
+  return (
+    <div className="search-engine">
+      <input
+        type="text"
+        placeholder="Enter city name"
+        name="search"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+}
+
+Search.propTypes = {
+  search: PropTypes.string,
+  setSearch: PropTypes.func,
+  handleSearch: PropTypes.func,
+};
